f191e4294447c86ca0603b7f966263220f43f4d9
Jesus <jesus.digon.abia@gmail.com>
2020-05-25 20:11:22 +0200

    cambios

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home  from './src/screens/home';
+import { render } from 'react-dom';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppNavigator from './src/navigation/AppNavigator';
 
-export default function App() {
+
+
+
+
+const Stack= createStackNavigator();
+
+
+class App extends React.Component {
+  render () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <React.Fragment>
+    <AppNavigator />
+    </React.Fragment>
+  )
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
